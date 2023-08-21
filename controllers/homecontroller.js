ecf770d931949d391b8077ddc2ad7a86b2fbd1c2
Pranav kulkarni <pnkulkarni2001@gmail.com>
2023-08-21 12:47:25 +0530

    some changes

--- controllers/homecontroller.js
+++ controllers/homecontroller.js
@@ -2,7 +2,29 @@ const Post=require('../models/posts');
 const User=require('../models/users');
 
 module.exports.home=function(req,res){
-    // console.log(req.cookies);
+   
+    Post.find({}).populate('user').populate({path:'comments',
+  populate:{path:'user'}}).then(function( posts){
+     User.find({}).then(function(users){
+      return res.render('home',{
+        title:"Codial/Home",
+        posts:posts,
+        all_users:users
+      });
+     }).catch(function(err){
+      console.log("Error in finding users");
+    });   
+  }).catch(function(err){
+    console.log("Error in finding posts");
+  });
+
+}
+
+
+
+
+
+ // console.log(req.cookies);
     // res.cookie('user_id',25)
  
     // Post.find({}).then(function( posts){
@@ -22,22 +44,3 @@ module.exports.home=function(req,res){
     //     }).catch(function(err){
     //       console.log("Error in finding posts");
     //     });
-
-    Post.find({}).populate('user').populate({path:'comments',
-  populate:{path:'user'}}).then(function( posts){
-     User.find({}).then(function(users){
-      return res.render('home',{
-        title:"Codial/Home",
-        posts:posts,
-        all_users:users
-      });
-     }).catch(function(err){
-      console.log("Error in finding users");
-    });   
-  }).catch(function(err){
-    console.log("Error in finding posts");
-  });
-   
-
-   
-}
